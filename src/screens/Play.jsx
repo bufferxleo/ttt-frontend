3384dd727292ef42bb7a6e0687f55c9d15dfabef
leocodeio <basicleo@787>
2024-07-23 10:35:28 +0530

    changed backend url

--- src/screens/Play.jsx
+++ src/screens/Play.jsx
@@ -17,7 +17,7 @@ const Play = () => {
   const handleStart = async (e) => {
     e.preventDefault();
     try {
-      const user = await axios.post("http://localhost:5001/user/create", {
+      const user = await axios.post(`${process.env.REACT_APP_API_URL}user/create`, {
         nickName: name,
       });
       setAccount(user.data.user);
